3a2cfb6375d747ea17e4f94c56f70e959ca05123
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-30 02:48:08 -0300

    feat: analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,13 +1,31 @@
-def most_request_maria(data):
-    
+from track_orders import TrackOrders
 
-def save_answers(data, report):
 
+def init_trackOrders(data):
+    list_orders = TrackOrders()
+    list_orders.__fill__(data)
+    return list_orders
+
+
+def save_answers(data):
+    list_or = init_trackOrders(data)
+    maria_most_ordered = list_or.get_most_ordered_dish_per_customer('maria')
+    times_ham = list_or. get_number_of_times_orderd('arnaldo', 'hamburguer')
+    joao_never_order = list_or.get_never_ordered_per_customer('joao')
+    joao_never_came = list_or.get_days_never_visited_per_customer('joao')
+    with open('data/mkt_campaign.txt', 'w') as file:
+        file.write(str(maria_most_ordered) + '\n')
+        file.write(str(times_ham) + '\n')
+        file.write(str(joao_never_order) + '\n')
+        file.write(str(joao_never_came) + '\n')
 
 
 def analyze_log(path_to_file):
     if not path_to_file.endswith('.csv'):
-        raise FileNotFoundError(f"Extenção inválida '{path_to_file}'")
-    with open(path_to_file) as file:
-        _data = file.readlines()
-    
+        raise FileNotFoundError(f"Extensão inválida '{path_to_file}'")
+    try:
+        with open(path_to_file, 'r') as file:
+            _data = file.readlines()
+        save_answers(_data)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
--- src/track_orders.py
+++ src/track_orders.py
@@ -8,6 +8,13 @@ class TrackOrders:
     def __len__(self):
         return len(self.orders)
 
+    def __fill__(self, list_orders):
+        for line in list_orders:
+            customer = line.split(',')[0]
+            order = line.split(',')[1]
+            day = line.split(',')[2].rstrip('\n')
+            self.add_new_order(customer, order, day)
+
     def add_new_order(self, customer, order, day):
         add_order = {
             'customer': customer,
@@ -19,6 +26,12 @@ class TrackOrders:
     def get_most_ordered_dish_per_customer(self, customer):
         order_list_cutomer = self.all_order_per_customer(customer, mode='arr')
         return Counter(order_list_cutomer).most_common(1)[0][0]
+
+    def get_number_of_times_orderd(self, customer, search):
+        order_list_cutomer = self.all_order_per_customer(customer, mode='arr')
+        for commom in Counter(order_list_cutomer).most_common():
+            if commom[0] == search:
+                return commom[1]
 
     def get_never_ordered_per_customer(self, customer):
         order_list_cutomer = self.all_order_per_customer(customer, mode='obj')
