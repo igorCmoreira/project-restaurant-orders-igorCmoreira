a077a048a963b642ebbc22f5919030d32a413fed
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-30 01:21:21 -0300

    feat: track_order

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,13 @@
+def most_request_maria(data):
+    
+
+def save_answers(data, report):
+
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f"Extenção inválida '{path_to_file}'")
+    with open(path_to_file) as file:
+        _data = file.readlines()
+    
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,72 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque 
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        add_order = {
+            'customer': customer,
+            'order': order,
+            'day': day
+        }
+        self.orders.append(add_order)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        order_list_cutomer = self.all_order_per_customer(customer, mode='arr')
+        return Counter(order_list_cutomer).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        order_list_cutomer = self.all_order_per_customer(customer, mode='obj')
+        orders = self.all_orders(mode='obj')
+        never = orders - order_list_cutomer
+        return never
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        open_days = self.operating_days(mode='obj')
+        visited_days = {
+            order['day'] for order in self.orders
+            if order['customer'] == customer
+        }
+        return open_days - visited_days
 
     def get_busiest_day(self):
-        pass
+        open_days = self.operating_days(mode='arr')
+        return Counter(open_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        open_days = self.operating_days(mode='arr')
+        return Counter(open_days).most_common()[-1][0]
+
+    def all_order_per_customer(self, customer, mode):
+        if mode == 'obj':
+            return {
+                order['order'] for order in self.orders
+                if order['customer'] == customer
+            }
+        return [
+                order['order'] for order in self.orders
+                if order['customer'] == customer
+            ]
+
+    def all_orders(self, mode):
+        if mode == 'obj':
+            return {
+                order['order'] for order in self.orders
+            }
+        return [
+                order['order'] for order in self.orders
+            ]
+
+    def operating_days(self, mode):
+        if mode == 'obj':
+            return {
+                order['day'] for order in self.orders
+            }
+        return [
+                order['day'] for order in self.orders
+            ]
